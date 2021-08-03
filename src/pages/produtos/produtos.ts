9d4801e498ad18049ca61f59bffbb69cc08e4dd9
heitor.ramos <heitorhfbr@gmail.com>
2021-08-03 13:31:25 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.cong';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -24,8 +25,19 @@ export class ProdutosPage {
     this.produtoService.findByCategorias(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrl();
       },
       error=>{});
-  
+    }
+
+    loadImageUrl(){
+      for(var i=0; i < this.items.length; i++){
+        let item = this.items[i];
+        this.produtoService.getSmallImagefromBucket(item.id)
+          .subscribe(response =>{
+            item.imgUrl = `${API_CONFIG.bucketBaseUrl}/produtos/prod${item.id}-small.jpg`
+          },
+          error=>{});
+      }
     }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { API_CONFIG } from "../../config/api.cong";
 
 @Injectable()
@@ -10,5 +11,10 @@ export class ProdutoService{
 
     findByCategorias(categoria_id : string){
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImagefromBucket(id : string) : Observable<any>{
+        let url = `${API_CONFIG.bucketBaseUrl}/produtos/prod${id}-small.jpg`
+        return this.http.get(url, {responseType : 'blob'});
     }
 }
